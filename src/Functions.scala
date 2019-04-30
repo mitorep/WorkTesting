0f0ecc71d03f0736769619d736d996d96563cf71
Michael <michaltorbinski@gmail.com>
2019-04-30 10:45:16 +0200

    loops, sort

--- src/Functions.scala
+++ src/Functions.scala
@@ -51,5 +51,44 @@ object Functions {
     for(friend <- friends2)
       println(friend)
 
+    for(i <- favNums.indices)
+      {
+        favNums(i) = i
+        println(i)
+      }
+
+    val favNumsTimes2 = for(num <- favNums) yield 2 * num
+   // favNumsTimes2.foreach(println)
+
+    var favNumsDiv4 = for(num <- favNums if num % 4 == 0) yield  num
+   // favNumsDiv4.foreach(println)
+
+    var multTable = Array.ofDim[Int](10,10)
+    for(i <- 0 to 9){
+      for(j <- 0 to 9){
+        multTable(i)(j) = i *j
+      }
+    }
+
+    for(i <- 0 to 9){
+      for(j <- 0 to 9){
+        printf("%d : %d = %d\n", i, j, multTable(i)(j))
+      }
+    }
+
+    for(i <- 0 to 9; j <- 0 to 9){
+
+        printf("%d : %d = %d\n", i, j, multTable(i)(j))
+
+    }
+
+    println("Sum :" + favNums.sum)
+    println("Min :" + favNums.min)
+    println("Max :" + favNums.max)
+
+    val sortedNums = favNums.sortWith(_>_)
+    //val sortedNums = favNums.sortWith(_>_)
+    sortedNums.foreach(println)
+
   }
 }
