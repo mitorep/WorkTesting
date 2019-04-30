c965c9b48d346c48fda30db3133abc1c49295146
Michael <michaltorbinski@gmail.com>
2019-04-30 15:08:38 +0200

    Exceptions

--- src/Exceptions.scala
+++ src/Exceptions.scala
@@ -1,3 +1,13 @@
 object Exceptions {
+  def main(args: Array[String]): Unit ={
+    def divideNums(num1 : Int, num2 : Int) = try{
+      (num1 / num2)
+    } catch {
+      case ex : java.lang.ArithmeticException => "Can't divide by zero"
+    } finally{
+      //clean up after exception
+    }
+    println("3 / 0 = " + divideNums(3, 0))
+  }
 
 }
