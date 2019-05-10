39dcda8bc15f2984182502e5c2afdc4dc17aec0f
Michal Torbinski <mtorbinski@regenersis.local>
2019-05-10 10:41:12 +0200

    edit file

--- src/Files.scala
+++ src/Files.scala
@@ -1,14 +1,18 @@
 import java.io.PrintWriter
 import scala.io.Source
+import java.io.BufferedWriter
+import java.io.FileWriter
 
 object Files {
 
   def main(args: Array[String]): Unit ={
-    val writer = new PrintWriter("test.txt")
-    writer.write("Just some random text\nSome more random text")
+    val path = """C:\Users\mtorbinski\Desktop\text.txt"""
+    val writer = new PrintWriter(new BufferedWriter(new FileWriter(path, true)));
+    writer.println("Just some random text.")
+    writer.println("Some more random text.")
     writer.close()
 
-    val textFromFile = Source.fromFile("test.txt", "UTF-8")
+    val textFromFile = Source.fromFile(path, "UTF-8")
 
     val lineIterator =  textFromFile.getLines()
 
